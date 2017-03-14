83cc8cf22b629f6d6767f7beee2f47c5b4c87522
Antoine Sueur <antoine.sueur@telecom-paristech.fr>
2017-03-14 16:57:03 +0100

    Corrections

--- script.py
+++ script.py
@@ -6,15 +6,35 @@ Created on Tue Mar 14 15:38:00 2017
 """
 
 import numpy as np
+import math
+
+
+######
+# Actions : 
+#    0 : duck, 1 : do nothing 2 : jump
+
+# constants
+n_inputs = 1
+n_outputs = 3
+n_hidden = 5
+path = "01212101210120"
+
+gene_size = 10
+dna_size = gene_size * (n_inputs + n_outputs )*n_hidden
+mutation_rate = 0.05
+crossover_ratio = 0.5
+cst_genome = list(range(0,dna_size,gene_size))
+number_crossover = 1
 
 #creates a neural network
-
-n_inputs = 3
-n_outputs = 2
-n_hidden = 3
 W1 = np.random.normal(size=(n_inputs, n_hidden))
-W2 = np.random.normal((n_hidden,n_outputs))
+W2 = np.random.normal(size = (n_hidden,n_outputs))
 
+
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+  
 #function to display a matrix
 
 def display_matrix(M):
@@ -29,45 +49,78 @@ def display_matrix(M):
                 print(' ',end="")
         print()
 
-def predict(arg):
-    return np.dot(arg,np.dot(W1,W2))
 
-def get_weights_from_genome(genome, gene_size):
-    n_gene = len(genome)/gene_size
-    gene_list = []
-    for i in range(n_gene):
-        gene_list.append(genome[i*gene_size: (i+1)*gene_size])
-    weight_list = []
-    for x in gene_list:
-        weight = binToInt(x)
-        weight_list.append(weight)
-    return weight_list
+def predict(arg, W1, W2):
+    intermediate = np.fromiter(map(sigmoid, np.dot(arg,W1)), dtype = float)
+    print(intermediate)
+    for x in map(sigmoid,np.dot(intermediate,W2)):
+        print(x)
+    return np.argmax(np.fromiter(map(sigmoid,np.dot(intermediate,W2)), dtype = float))
 
+    
+# a modifier pour négatifs à virgule 
 def binToInt(bin_list):
     sumInt = sum([x*(2**i) for (i,x) in enumerate(bin_list)])
     n = len(bin_list)
     return sumInt - bin_list[n-1] * (2**n)
 
-print(binToInt([1,0,0,0,1,0]))
-display_matrix(W1)
 
-class Runner:
-  def __init__(self, entity_id):
-    	self.entity_id = entity_id
-      
-  def fill_DNA(DNA):
-    self.DNA = DNA
+class Individual:
+    def __init__(self, entity_id, dna_size, genome):
+        self.entity_id = entity_id
+        self.dna_size = dna_size
+        self.genome = genome
     
-    
-class Population:
-  runner_list = []
-  def __init__(self, pop_size):
-    self.pop_size = pop_size
-    for i in range(pop_size):
-		r = Runner(i)
-      	#r.fill_DNA(np.random.randint(2,size = ))
-      	self.runner_list.append(r)
+    def generate_random(self):
+        self.DNA = np.random.randint(2,size = self.dna_size)
         
+    def mutate(self):
+        for i in range(len(self.DNA)):
+            rand_int = np.random.rand_sample(1)[0]
+            if(rand_int < mutation_rate):
+                self.DNA[i] = np.random.randint(2)
+                
+    def crossover(self,mother, father):
+        #   computing random crossover points
+        Loci_crossover = np.random.sample(range(1,self.dna_size), number_crossover)
+        Loci_crossover = [0] + sorted(Loci_crossover)
+        Loci_crossover.append(self.dna_size)
+        # print Loci_crossover
+        # the child's DNA will be read alternatively from parent1 and parent2
+        parent1 = mother.DNA
+        parent2 = father.DNA
+        if np.random.randint(0,1):    # starting indifferently from mother or father
+            parent1, parent2 = parent2, parent1     # swapping parents
+        self.DNA = []
+        for cut_point in range(len(Loci_crossover)-1):
+            self.DNA += list(parent1[Loci_crossover[cut_point]:Loci_crossover[cut_point+1]])
+            parent1, parent2 = parent2, parent1     # swapping parents        
+
+        
+class Population:
+    
+    runner_list = []
+  
+    def __init__(self, pop_size):
+        self.pop_size = pop_size
+        for i in range(pop_size):
+            r = Individual(i,dna_size, cst_genome)
+            r.generate_random()
+            self.runner_list.append(r)
+        
+    
         
 def genome_to_weights(genome):
-  return weights
+    return 0
+    
+    
+def score(individual):
+    i = 0
+    go_on = True
+    while(go_on):
+        action = predict(int(path[i]), W1, W2)
+        if(not(action == path[i] or path[i] == 1)):
+            go_on = False
+        else:
+            i+=1
+    return i
